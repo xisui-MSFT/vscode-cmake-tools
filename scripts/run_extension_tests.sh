f4c73556cd16abcaa40caba81dbc3c3c8171a3ce
vector-of-bool <vectorofbool@gmail.com>
2018-03-10 18:13:36 -0700

    Simpler test-runner bash script, and a test to check that builds run for all Platforms

--- scripts/run_extension_tests.sh
+++ scripts/run_extension_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-pushd .
+set -eu
+
+export HasVs=false
+export CMT_TESTING=1
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
@@ -9,54 +12,21 @@ else
 	ROOT=$(dirname $(dirname $(readlink -f $0)))
 fi
 
-cd $ROOT
-
-export HasVs=false
-export CMT_TESTING=1
+pushd $ROOT
 
 # Run all tests with a workspace folder where no CMakeLists.txt exists
 # This prevents automatic loading of the extension.
-export CODE_TESTS_PATH=$ROOT/out/test/extension-tests/without-cmakelist-file
-export CODE_TESTS_WORKSPACE=$ROOT/test/extension-tests/without-cmakelist-file/project-folder
-node ./node_modules/vscode/bin/test
-test_error_code=$?
 
-export CODE_TESTS_PATH=$ROOT/out/test/extension-tests/successful-build
-export CODE_TESTS_WORKSPACE=$ROOT/test/extension-tests/successful-build/project-folder
-node ./node_modules/vscode/bin/test
-test_error_code_2=$?
-
-popd
-
-# Forward error level
-if [ $test_error_code -ne 0 ]; then
-	exit $test_error_code
-fi
-
-pushd .
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
-	ROOT=$(dirname $(dirname $(realpath "$0")))
-else
-	ROOT=$(dirname $(dirname $(readlink -f $0)))
-fi
-
-cd $ROOT
-
-export HasVs=false
-export CMT_TESTING=1
-
-# Run all tests with a workspace folder where no CMakeLists.txt exists
-# This prevents automatic loading of the extension.
-export CODE_TESTS_PATH=$ROOT/out/test/extension-tests/successful-build
-export CODE_TESTS_WORKSPACE=$ROOT/test/extension-tests/successful-build/project-folder
-node ./node_modules/vscode/bin/test
-test_error_code=$?
+TESTS=(
+	without-cmakelist-file
+	successful-build
+	vs-preferred-gen  # Skipped on non-Windows, but listed here for completeness.
+)
+for testname in ${TESTS[@]}; do
+	env \
+		CODE_TESTS_PATH=$ROOT/out/test/extension-tests/$testname \
+		CODE_TESTS_WORKSPACE=$ROOT/test/extension-tests/$testname/project-folder \
+		node ./node_modules/vscode/bin/test
+done
 
 popd
-
-# Forward error level
-if [ $test_error_code -ne 0 ]; then
-	exit $test_error_code
-fi
